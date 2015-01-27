9a6f3baace0d1b8f8361a4ef546cf9455501e744
marqh <markh@scitools.org.uk>
2015-01-27 13:47:59 +0000

    redirects

--- lib/metarelate/editor/settings.py
+++ lib/metarelate/editor/settings.py
@@ -159,7 +159,7 @@ AUTHENTICATION_BACKENDS = (
 
 AUTH_USER_MODEL = 'app.CustomUser'
 LOGIN_URL = '/metocean/login/'
-LOGIN_REDIRECT_URL = '/metocean/home/'
+LOGIN_REDIRECT_URL = '/metocean/login/'
 URL_PATH = ''
 SOCIAL_AUTH_STRATEGY = 'social.strategies.django_strategy.DjangoStrategy'
 SOCIAL_AUTH_STORAGE = 'social.apps.django_app.default.models.DjangoStorage'
