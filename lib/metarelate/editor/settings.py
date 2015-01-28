e72c223ffacaa5f8e68975873e1d931884bbd742
marqh <markh@metarelate.net>
2015-01-28 17:02:06 +0000

    set log

--- lib/metarelate/editor/settings.py
+++ lib/metarelate/editor/settings.py
@@ -121,28 +121,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler'
-#         }
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['mail_admins'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#     }
-# }
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -155,6 +133,11 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'metarelate.editor.app.views': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,
